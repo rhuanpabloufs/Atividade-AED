39c26c19964d33c97303b7eb5b159e3fc7a2ade7
rhuanpabloufs <rhuan.santos@dcomp.ufs.br>
2026-04-16 21:01:54 -0300

    Update quest3PA.java

--- quest3PA.java
+++ quest3PA.java
@@ -1,1 +1,12 @@
-
+public class Node {
+  public Object item ; // dado armazenado ( generico )
+  public Node next ; // referencia ao proximo no
+  public Node(Object item){
+      this.item = item;
+      this.next = null;
+  }
+  public Node(Object item, Node next){
+    this.item = item;
+    this.next = next;
+  }
+}
