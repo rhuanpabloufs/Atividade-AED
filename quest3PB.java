aca0c44aa9cea31d22785a0887e829bbed2f989d
rhuanpabloufs <rhuan.santos@dcomp.ufs.br>
2026-04-16 21:07:27 -0300

    Add simple linked list implementation
    Implemented a simple linked list with methods to insert at the beginning and print elements.

--- quest3PB.java
+++ quest3PB.java
@@ -1,1 +1,37 @@
 
+public class ListaSimples {
+ private Node head ;
+
+ public ListaSimples () {
+   this.head = null ;
+ }
+
+ /** Insere ’item ’ no INICIO da lista (O (1) ). */
+ public void insertFirst ( Object item ) {
+    if(head == null){
+       head = new Node(item);
+    } else {
+       Node novo = new Node(item, head);
+       head = novo;
+    }
+ }
+
+ /** Percorre a lista e imprime todos os elementos . */
+ public void imprimir () {
+   Node atual = head ;
+   while ( atual != null ) {
+   System.out.print ( atual . item + " -> ") ;
+   atual = atual.next ;
+ }
+   System . out . println (" null ") ;
+
+ }
+
+ public static void main ( String [] args ) {
+   ListaSimples lista = new ListaSimples () ;
+   lista.insertFirst ("C") ;
+   lista.insertFirst ("B") ;
+   lista.insertFirst ("A") ;
+   lista.imprimir () ; // A -> B -> C -> null
+ }
+}
